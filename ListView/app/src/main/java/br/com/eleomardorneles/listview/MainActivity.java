f7bc1e4ff57273ebacd60cf6fc8dc3be30f95208
oeleomar <eleomar.dev@gmail.com>
2023-04-01 20:55:51 -0400

    Final list view

--- ListView/app/src/main/java/br/com/eleomardorneles/listview/MainActivity.java
+++ ListView/app/src/main/java/br/com/eleomardorneles/listview/MainActivity.java
@@ -1,14 +1,49 @@
 package br.com.eleomardorneles.listview;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private ListView listLocais;
+    private String[] itens = {
+            "Angra dos Reis", "Caldas Novas",
+            "Campos do Jordão", "Costa do Sauípe",
+            "Ilhéus", "Porto Seguro", "Tiradentes",
+            "Praga", "Santiago", "Zurique", "Caribe",
+            "Buenos Aires", "Budapeste", "Cancún", "Aruba"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.listLocais = findViewById(R.id.listLocais);
+
+        //Criar adaptador para a lista
+        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(
+                getApplicationContext(),
+                android.R.layout.simple_list_item_1,
+                android.R.id.text1,
+                itens
+        );
+
+        //Adiciona adaptador para a lista
+        listLocais.setAdapter(adaptador);
+
+        //Adicionar click na lista
+        listLocais.setOnItemClickListener((new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String valorSelecionado = listLocais.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), valorSelecionado ,Toast.LENGTH_SHORT).show();
+            }
+        }));
     }
 }
