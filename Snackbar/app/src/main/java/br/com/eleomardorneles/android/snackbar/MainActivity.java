f33542e4fe0692f20cd70c9310faa9c149c88513
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2023-04-03 17:08:10 -0400

    Final add Snackbar

--- Snackbar/app/src/main/java/br/com/eleomardorneles/android/snackbar/MainActivity.java
+++ Snackbar/app/src/main/java/br/com/eleomardorneles/android/snackbar/MainActivity.java
@@ -3,12 +3,34 @@ package br.com.eleomardorneles.android.snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Botão pressionado", Snackbar.LENGTH_SHORT)
+                        .setAction("Confirmar", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                button.setText("Botão abrir alterado");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.purple_200))
+                        .show();
+            }
+        });
     }
 }
